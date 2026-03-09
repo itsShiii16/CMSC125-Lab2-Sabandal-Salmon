27b17da475c605feca382ab06d49728cf49bfa46
itsShiii16 <slsabandal@up.edu.ph>
2026-03-09 23:42:50 +0800

    feat: add average scheduling metrics calculation

--- schedsim/src/fcfs.c
+++ schedsim/src/fcfs.c
@@ -5,7 +5,6 @@
 #include "../include/process.h"
 #include "../include/scheduler.h"
 
-
 /* Helper function for sorting by arrival time */
 static int compare_arrival(const void *a, const void *b) {
     Process *p1 = (Process *)a;
@@ -18,14 +17,12 @@ static int compare_arrival(const void *a, const void *b) {
     return p1->arrival_time - p2->arrival_time;
 }
 
-
 /* FCFS Scheduler */
 void schedule_fcfs(SchedulerState *state) {
 
     Process *processes = state->processes;
     int n = state->process_count;
 
-    /* Sort processes by arrival time */
     qsort(processes, n, sizeof(Process), compare_arrival);
 
     state->current_time = 0;
@@ -37,31 +34,23 @@ void schedule_fcfs(SchedulerState *state) {
 
         Process *p = &processes[i];
 
-        /* If CPU is idle, jump to arrival time */
         if (state->current_time < p->arrival_time) {
             state->current_time = p->arrival_time;
         }
 
-        /* Start time */
         p->start_time = state->current_time;
 
-        /* Execute process */
         state->current_time += p->burst_time;
 
-        /* Finish time */
         p->finish_time = state->current_time;
 
-        /* Metrics */
         p->turnaround_time = p->finish_time - p->arrival_time;
         p->waiting_time = p->turnaround_time - p->burst_time;
         p->response_time = p->start_time - p->arrival_time;
 
-        /* Process fully executed */
         p->remaining_time = 0;
     }
 
-
-    /* Print results */
     printf("\nResults\n");
     printf("-----------------------------------------------------\n");
     printf("PID\tAT\tBT\tST\tFT\tTT\tWT\tRT\n");
@@ -81,4 +70,22 @@ void schedule_fcfs(SchedulerState *state) {
                p->response_time);
     }
 
+    /* Calculate averages */
+    double total_tt = 0;
+    double total_wt = 0;
+    double total_rt = 0;
+
+    for (int i = 0; i < n; i++) {
+        total_tt += processes[i].turnaround_time;
+        total_wt += processes[i].waiting_time;
+        total_rt += processes[i].response_time;
+    }
+
+    double avg_tt = total_tt / n;
+    double avg_wt = total_wt / n;
+    double avg_rt = total_rt / n;
+
+    printf("\nAverage Turnaround Time: %.2f\n", avg_tt);
+    printf("Average Waiting Time   : %.2f\n", avg_wt);
+    printf("Average Response Time  : %.2f\n", avg_rt);
 }
