3402b8ace375cfe56c61018167a7ab0bde3106b9
g4biruu <aisalmon@up.edu.ph>
2026-03-16 04:22:39 +0800

    feat: add schedule validation module skeleton

---
+++ schedsim/src/checker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../include/checker.h"
+
+int validate_schedule(Process processes[], int count)
+{
+    int valid = 1;
+
+    for (int i = 0; i < count; i++) {
+        Process *p = &processes[i];
+    }
+
+    return valid;
+}
